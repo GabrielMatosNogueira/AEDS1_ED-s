b2afe86db36e0d06f2b92190bfde2eb08eed1ce0
Gabriel Matos Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-04-07 17:58:13 -0300

    Update ED-AA1.c

--- ED-AA1.c
+++ ED-AA1.c
@@ -212,9 +212,9 @@ bool method_02_auxiliar(void)
     // Declaração de variáveis
     bool condicional = true;
     int quantidade = 0;
-    char caractere_1 = 'a';
-    char caractere_2 = 'b';
-    char caractere_3 = 'c';
+    char caractere_1 = ' ';
+    char caractere_2 = ' ';
+    char caractere_3 = ' ';
 
 
     quantidade = IO_readint("Digite a quantidade de vezes para testar: ");
@@ -280,39 +280,53 @@ void method_03(void)
     int b=0;
     int i=0;
     int quantidade=0;
+    int contador=0;
     int *numeros=0;
-    int *numerosRejeitados=0;
     // [a, b)
 
     // Leitura dos limites
     IO_print("Formatacao: [a,b)");
     a=IO_readint("\nDigite o valor do limite inferior [FECHADO]: ");
     b=IO_readint("\nDigite o valor do limite superior (ABERTO): ");
+    quantidade=IO_readint("\nDigite a quantidade de testes: ");
 
     // Malloc para alocar outros 'n' termos
     numeros= malloc(sizeof(int)*quantidade);
-    numerosRejeitados= malloc(sizeof(int)*quantidade);
 
     for (i=0; i<quantidade; i=i+1)
     {
-        numeros=IO_readint("Digite um numero para testar no intervalo: ");
-
-        if ( numeros >=a && numeros <b )
-        {
-            numeros[i]=numeros;
-        }
-        else
-        {
-            numerosRejeitados[i]= numeros;
-        }
+        printf("\n[%d]. Digite um numero para testar no intervalo: ", i+1);
+        scanf("%d", &numeros[i]);
+        getchar( );
     }
 
-    // Mostrar os numeros
-    for (i=0; i<quantidade; i =)
-    printf("\nNumeros dentro do intervalo")
+    /*
+
+            TABELA              TABELA
+        1           25  |   1           1   |
+        2           27  |   2           2   |
+        3           30  |   3           3   |  
+        4           35  |   4           4   |
+        5           37  |   5           5   |
+    
+    */
+
+    {    
+
+        if ( numeros[i] >= a && numeros[i] < b )
+        {
+            numeros[i];
+            contador = contador+1;
+            printf("[%d]", i);
+        }
+        
+    }
+
+    // Print dos numeros do contador
+    printf("\nNumeros dentro do intervalo: [%d]", contador);
 
     // Encerramento
-    printf("%s\n", "\nApertar ENTER para continuar\n");
+    printf("\n%s\n", "Apertar ENTER para continuar\n");
     getchar();
 }
 
