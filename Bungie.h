a253d2ad4979ab4eaaef40c9dfa5456ef4529b65
Gabriel Matos Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-04-09 19:50:03 -0300

    Update Bungie.h

--- Bungie.h
+++ Bungie.h
@@ -1,5 +1,5 @@
 /*
-BUNGIE LIBRARY
+[BUNGIE LIBRARY]
 - Type: Input/Output console
 - Name: Bungie
 - @author GabrielMatosNogueira [Github]
@@ -8,7 +8,7 @@ BUNGIE LIBRARY
 [RESPONSABLE CLEAN CODE, PLEASE CHECKOUT LIBRARY DOCUMENTATION]
 */
 
-// Bibliotecas:
+// Libraries:
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,7 +18,7 @@ BUNGIE LIBRARY
 #include <time.h>
 #include <time.h>
 
-// Exibithion functions
+// EXIBITHION FUNCTIONS
 
 // ED-s identification
 void Iidentification (const char *ED)
@@ -26,10 +26,47 @@ void Iidentification (const char *ED)
     printf("\n- [%s]\n- Author: Gabriel Matos Nogueira\n- Registration: 870863\n\n", ED);
 }
 
-// Read data functions
-void Ireadint (int number)
+// Display on screen
+void Iprint(const char *phrase)
 {
-    int scannumber=0;
-    scanf("%d", &scannumber);
-    getchar()
+    printf("%s", phrase);
 }
+
+// Display message with one integer value
+void IprintInt(const char *phrase, int number)
+{
+    printf("%s[%d]", phrase, number);
+}
+
+
+// READ DATA FUNCTIONS
+
+// Read integer value
+int Ireadint (int number)
+{
+    int scan=0;
+    scanf("%d", &scan);
+    getchar();
+    return (scan);
+}
+
+// Read double value
+double Ireaddouble (double number)
+{
+    double scan=0.0;
+    scanf("%lf", &scan);
+    getchar();
+    return (scan);
+}
+
+// Read char value
+
+char Ireadchar (char character)
+{
+    char letter=' ';
+    scanf("%c", &letter);
+    getchar();
+    return (letter);
+}
+
+// MATH FUNCTIONS
